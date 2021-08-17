583072d2a272b26aa3b64cd9fa3f34159aa53ad5
taffarel55 <mauruciotaffarel@gmail.com>
2021-08-17 10:22:05 -0300

    Adicionando pesos sinápticos

--- neuro.js
+++ neuro.js
@@ -1,3 +1,14 @@
-class Neuronio {
-
+class Neurônio {
+  constructor(w1, w2) {
+    (this.w1 = w1 ?? numeroAleatorio(0, 1)),
+      (this.w2 = w2 ?? numeroAleatorio(0, 1));
+  }
 }
+
+function numeroAleatorio(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+const n = new Neurônio();
+
+console.log(n);
